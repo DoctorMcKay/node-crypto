42904ac22b45b388b39fcf3e02f2c97ee4547814
Alexander Corn <mckay@doctormckay.com>
2017-10-15 04:07:02 -0400

    Store test key in const TEST_KEY

--- test.js
+++ test.js
@@ -1,29 +1,29 @@
-var McCrypto = require('./index.js');
+const McCrypto = require('./index.js');
+
+const TEST_KEY = "foo bar baz";
 
 legacyAES256CTRTest();
 expectAuthenticationTest();
 
-var testKey = "foo bar baz";
-
 // Ensure everything works with strings
 var testData = "The quick brown fox jumps over the lazy dog.";
-encryptAndDecryptTest(testKey, testData, "strings");
+encryptAndDecryptTest(testData, "strings");
 
 
 // Ensure everything works with buffers
 testData = new Buffer("The quick brown fox jumps over the lazy dog.", "ascii");
-encryptAndDecryptTest(testKey, testData, "buffers");
+encryptAndDecryptTest(testData, "buffers");
 
 
-function encryptAndDecryptTest(key, data, testType) {
-	var encrypted = McCrypto.encrypt(McCrypto.Cipher.AES256CTRWithHMAC, key, data);
+function encryptAndDecryptTest(data, testType) {
+	var encrypted = McCrypto.encrypt(McCrypto.Cipher.AES256CTRWithHMAC, TEST_KEY, data);
 	console.log(encrypted);
 	if (!McCrypto.isWellFormed(encrypted)) {
 		throw new Error("Encrypted data is not well-formed!");
 	}
 
 	// Make sure it decrypts
-	var decrypted = McCrypto.decrypt(key, encrypted, true);
+	var decrypted = McCrypto.decrypt(TEST_KEY, encrypted, true);
 	var isEqual = decrypted.equals ? decrypted.equals(data) : decrypted == data;
 	if (!isEqual) {
 		console.log(decrypted);
@@ -40,7 +40,7 @@ function encryptAndDecryptTest(key, data, testType) {
 function expectAuthenticationTest() {
 	var encrypted = new Buffer("fade01011093480061d6340d2c298bfc5b4e98d661d1957a647b1045088f14592fd35784c8e2a32727ff639276092cd45e7412b47d7d21eebec6944b778943fc6b", "hex");
 	try {
-		McCrypto.decrypt("foo bar baz", encrypted, true);
+		McCrypto.decrypt(TEST_KEY, encrypted, true);
 	} catch (ex) {
 		if (ex.message == "Expected authentication, but data was encrypted with AES256CTR without HMAC") {
 			console.log("expectAuthentication passed");
@@ -55,7 +55,7 @@ function expectAuthenticationTest() {
 
 function legacyAES256CTRTest() {
 	var encrypted = new Buffer("fade01011093480061d6340d2c298bfc5b4e98d661d1957a647b1045088f14592fd35784c8e2a32727ff639276092cd45e7412b47d7d21eebec6944b778943fc6b", "hex");
-	if (McCrypto.decrypt("foo bar baz", encrypted) != "The quick brown fox jumps over the lazy dog.") {
+	if (McCrypto.decrypt(TEST_KEY, encrypted) != "The quick brown fox jumps over the lazy dog.") {
 		throw new Error("Legacy AES256CTR decryption failed");
 	} else {
 		console.log("Legacy AES256CTR decryption passed");
